bd5dd28119f634e821240618412d310ac9ffd77b
Cedric-Font <font.cedric38@gmail.com>
2023-10-24 16:14:37 +0200

    up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,3 @@
-import { useState } from "react";
-import logo from "./assets/logo.svg";
-import UserId from "./components/UserId/UserId";
-import Api from "./components/Api/Api";
-import Answers from "./components/Answers/Answers";
-import Image from "./components/Image/Image";
-
 import "./App.css";
 
 function App() {
