55e1a32ee09093413c85a91d8dbec421108bd62f
Cedric-Font <font.cedric38@gmail.com>
2023-10-24 16:11:57 +0200

    fix all bug with prettier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from "react";
 import logo from "./assets/logo.svg";
-import UserId from "./components/UserId/UserId"
-import Api from "./components/Api/Api"
-import Answers from "./components/Answers/Answers"
-import Image from "./components/Image/Image"
+import UserId from "./components/UserId/UserId";
+import Api from "./components/Api/Api";
+import Answers from "./components/Answers/Answers";
+import Image from "./components/Image/Image";
 
 import "./App.css";
 
 function App() {
-  return (
-   <p>jhgjhg</p>
-  )
+  return <p>jhgjhg</p>;
 }
 
 export default App;
