8f601f1111583011e8b0e2c0662d82b8bd92c41f
Cedric-Font <font.cedric38@gmail.com>
2023-10-24 15:40:53 +0200

    add fil

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,16 @@
-import Counter from "./components/Counter";
+import { useState } from "react";
 import logo from "./assets/logo.svg";
+import UserId from "./components/UserId/UserId"
+import Api from "./components/Api/Api"
+import Answers from "./components/Answers/Answers"
+import Image from "./components/Image/Image"
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React !</p>
-
-        <Counter />
-
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+   
+  )
 }
 
 export default App;
