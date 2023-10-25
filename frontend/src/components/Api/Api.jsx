06fd69d795d621180050334638a521abcbd6d8e3
Christopher Arthaud <christopher.arthaud@gmail.com>
2023-10-25 14:21:30 +0200

    fetch used instead of ky

--- frontend/src/components/Api/Api.jsx
+++ frontend/src/components/Api/Api.jsx
@@ -1,18 +1,16 @@
-import ky from "ky";
 import { useEffect } from "react";
 import PropTypes from "prop-types";
 
 function Api({ film, setFilm }) {
   useEffect(() => {
-    const alea = Math.floor(Math.random() * 20);
-    const aleaPage = Math.floor(Math.random() * 10 + 1);
+    const aleaPage = Math.floor(Math.random() * 10) + 1; // génère un nombre aléatoire entre 1 et 10 pour choisir aléatoirement une page lors du fetch
+    const alea = Math.floor(Math.random() * 19); // génère un nombre aléatoire entre 0 et 19 pour choisir un seul film dans le fichier issu du fetch
     (async () => {
-      const parsed = await ky
-        .get(
-          `https://api.themoviedb.org/3/discover/movie?api_key=d8bedc0b2557b9f57527c89f46531039&language=fr-FR&page=${aleaPage}&sort_by=revenue.desc`
-        )
-        .json();
-      setFilm(parsed.results[alea]);
+      const movie = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=d8bedc0b2557b9f57527c89f46531039&language=fr-FR&page=${aleaPage}&sort_by=revenue.desc`
+      );
+      const movieParsed = await movie.json();
+      setFilm(movieParsed.results[alea]);
     })();
   }, []);
 
