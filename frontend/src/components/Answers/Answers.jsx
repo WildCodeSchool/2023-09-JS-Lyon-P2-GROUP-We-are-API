c2e499e5d35d2d9cfe1453337eb7515b6f0b2325
Cedric-Font <font.cedric38@gmail.com>
2023-11-09 15:07:58 +0100

    fix eslint

--- frontend/src/components/Answers/Answers.jsx
+++ frontend/src/components/Answers/Answers.jsx
@@ -14,6 +14,7 @@ export default function Answers({
     setCheck(true);
     input.value = "";
   }
+  // test
   return (
     <div className={styles.appAnswer}>
       <label htmlFor="Answers">Answer:</label>
