b169ada4119841c4939ede4afca8d48bc8222ab8
Christopher Arthaud <christopher.arthaud@gmail.com>
2023-11-17 13:11:01 +0100

    fix API call bugs

--- frontend/src/components/Api/Api.jsx
+++ frontend/src/components/Api/Api.jsx
@@ -1,41 +1,33 @@
 import { useEffect } from "react";
 import PropTypes from "prop-types";
 
-function Api({ film, setFilm, setAnswersReturn, setNext, reset, setPlease }) {
+function Api({ setFilm, setAnswersReturn, setNext, reset, setPlease }) {
   useEffect(() => {
-    function apiCall() {
-      const aleaPage = Math.floor(Math.random() * 10) + 1; // génère un nombre aléatoire entre 1 et 10 pour choisir aléatoirement une page lors du fetch
-      const alea = Math.floor(Math.random() * 19); // génère un nombre aléatoire entre 0 et 19 pour choisir un seul film dans le fichier issu du fetch
-      (async () => {
-        const movie = await fetch(
-          `https://api.themoviedb.org/3/discover/movie?api_key=d8bedc0b2557b9f57527c89f46531039&language=fr-FR&page=${aleaPage}&sort_by=revenue.desc`
-        );
-        const movieParsed = await movie.json();
-        setFilm(movieParsed.results[alea]);
-      })();
-    }
-    if (
-      film.original_language !== "fr" ||
-      film.original_language !== "en" ||
-      film.backdrop_path === null
-    ) {
-      apiCall();
-    }
+    const aleaPage = Math.floor(Math.random() * 10) + 1; // génère un nombre aléatoire entre 1 et 10 pour choisir aléatoirement une page lors du fetch
+    const alea = Math.floor(Math.random() * 19); // génère un nombre aléatoire entre 0 et 19 pour choisir un seul film dans le fichier issu du fetch
+    (async () => {
+      const movie = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&api_key=d8bedc0b2557b9f57527c89f46531039&language=fr-FR&page=${aleaPage}&sort_by=revenue.desc&with_original_language=en|fr`
+      );
+      const movieParsed = await movie.json();
+      setFilm(movieParsed.results[alea]);
+    })();
+
     setAnswersReturn(false);
     setNext(false);
     setPlease(false);
     const element = reset;
     element.current = true;
   }, []);
-}
 
-Api.propTypes = {
-  setAnswersReturn: PropTypes.func.isRequired,
-  setFilm: PropTypes.func.isRequired,
-  setPlease: PropTypes.func.isRequired,
-  reset: PropTypes.exact({
-    current: PropTypes.bool.isRequired,
-  }),
-};
+  Api.propTypes = {
+    setAnswersReturn: PropTypes.func.isRequired,
+    setFilm: PropTypes.func.isRequired,
+    setPlease: PropTypes.func.isRequired,
+    reset: PropTypes.exact({
+      current: PropTypes.bool.isRequired,
+    }),
+  };
+}
 
 export default Api;
