8c8250084df4329bbcafa85c0ba1eeefd76bf785
Christopher Arthaud <christopher.arthaud@gmail.com>
2023-11-18 02:25:11 +0100

    propType fixed

--- frontend/src/components/ImageComponent/ImageComponent.jsx
+++ frontend/src/components/ImageComponent/ImageComponent.jsx
@@ -1,4 +1,3 @@
-import PropTypes from "prop-types";
 import { useOutletContext } from "react-router-dom";
 import Image from "../Image/Image";
 
@@ -17,36 +16,5 @@ function ImageComponent() {
     />
   );
 }
-
-ImageComponent.defaultProps = {
-  film: { title: "Veuillez attendre votre film" },
-  reset: {
-    current: "waiting",
-  },
-};
-ImageComponent.propTypes = {
-  film: PropTypes.exact({
-    title: PropTypes.string,
-    backdrop_path: PropTypes.string,
-    adult: PropTypes.bool,
-    genre_ids: PropTypes.array,
-    id: PropTypes.number,
-    original_language: PropTypes.string,
-    original_title: PropTypes.string,
-    overview: PropTypes.string,
-    popularity: PropTypes.number,
-    poster_path: PropTypes.string,
-    release_date: PropTypes.string,
-    video: PropTypes.bool,
-    vote_average: PropTypes.number,
-    vote_count: PropTypes.number,
-  }),
-  setNext: PropTypes.func.isRequired,
-  next: PropTypes.func.isRequired,
-  reset: PropTypes.exact({
-    current: PropTypes.bool.isRequired,
-  }),
-  userId: PropTypes.func.isRequired,
-};
 
 export default ImageComponent;
--- frontend/src/components/SynopsisComponent/SynopsisComponent.jsx
+++ frontend/src/components/SynopsisComponent/SynopsisComponent.jsx
@@ -1,4 +1,3 @@
-import PropTypes from "prop-types";
 import { useOutletContext } from "react-router-dom";
 import Synopsis from "../Synopsis/Synopsis";
 
@@ -16,36 +15,4 @@ function SynopsisComponent() {
     />
   );
 }
-
-SynopsisComponent.defaultProps = {
-  film: { title: "Veuillez attendre votre film" },
-  reset: {
-    current: "waiting",
-  },
-};
-SynopsisComponent.propTypes = {
-  film: PropTypes.exact({
-    title: PropTypes.string,
-    backdrop_path: PropTypes.string,
-    adult: PropTypes.bool,
-    genre_ids: PropTypes.array,
-    id: PropTypes.number,
-    original_language: PropTypes.string,
-    original_title: PropTypes.string,
-    overview: PropTypes.string,
-    popularity: PropTypes.number,
-    poster_path: PropTypes.string,
-    release_date: PropTypes.string,
-    video: PropTypes.bool,
-    vote_average: PropTypes.number,
-    vote_count: PropTypes.number,
-  }),
-  setNext: PropTypes.func.isRequired,
-  next: PropTypes.func.isRequired,
-  reset: PropTypes.exact({
-    current: PropTypes.bool.isRequired,
-  }),
-  userId: PropTypes.func.isRequired,
-};
-
 export default SynopsisComponent;
